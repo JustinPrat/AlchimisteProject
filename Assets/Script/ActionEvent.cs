c93c2246b5f385c7687cc6c18f181069f8be9389
LeoLelann <112940420+LeoLelann@users.noreply.github.com>
2024-10-15 10:12:30 +0200

    [ADD] Animation fiole proto

--- Assets/Script/ActionEvent.cs
+++ Assets/Script/ActionEvent.cs
@@ -7,5 +7,40 @@ using UnityEngine.InputSystem;
 public class ActionEvent : MonoBehaviour
 {
     InputAction inputAction;
+    PlayerInput input;
+    Animation fiole1Anim;
+    Animator animator;
 
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    public void OnUse1(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            //float value = context.ReadValue<float>(); 
+            //Animation = value
+            animator.SetTrigger("Fiole1");
+
+        }
+    }
+    public void OnUse2(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            float value = context.ReadValue<float>();
+            //Animation = value
+
+        }
+    }
+    public void OnUse3(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            float value = context.ReadValue<float>();
+            //Animation = value
+
+        }
+    }
 }
